ea5cb96ffef9b71aad8b77a0f1f01a2dfe87d05c
RAMBABU001 <p.rambabu00000@gmail.com>
2026-05-16 02:10:34 +0530

    Fixed Gmail SMTP timeout

--- backend/server.js
+++ backend/server.js
@@ -50,17 +50,17 @@ app.post("/send-otp", async (req, res) => {
 const transporter =
   nodemailer.createTransport({
 
-    host: "smtp.gmail.com",
-
-    port: 587,
-
-    secure: false,
+    service: "gmail",
 
     auth: {
 
       user: process.env.EMAIL_USER,
 
       pass: process.env.EMAIL_PASS
+    },
+
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
